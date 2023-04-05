ca417e7bde3d662d1a0b8ec7098e4e93a57f9f3e
MiguelAngelCastro <mikeangel7879@gmail.com>
2023-04-04 23:13:28 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'UTT_BD_PROYECTO_WEB';
+}
